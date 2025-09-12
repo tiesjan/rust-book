8ac36b192cc097e6907fac3477bfa0bb113fbd1f
Ties Jan Hefting <hello@tiesjan.com>
2025-09-12 18:00:27 +0200

    First part of Chapter 3

---
+++ chapter-03/variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // Shadowing with same type
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    println!();
+
+    // Shadowing with different type
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("There are {spaces} spaces.")
+}
